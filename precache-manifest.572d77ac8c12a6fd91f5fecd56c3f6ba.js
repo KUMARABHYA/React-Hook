867206019d988c58fd484a85cd9d63063a7fb6b9
KUMARABHYA <Abhyanand@tothenew.com>
2021-04-01 16:29:44 +0530

    Updates

---
+++ precache-manifest.572d77ac8c12a6fd91f5fecd56c3f6ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf061f59fced2333a0ef0aa4bec044cf",
+    "url": "/React-Hook/index.html"
+  },
+  {
+    "revision": "cfbd7e8d931fd3621614",
+    "url": "/React-Hook/static/css/main.55b2f47f.chunk.css"
+  },
+  {
+    "revision": "cf32022a42377e50d0ed",
+    "url": "/React-Hook/static/js/2.728ff00a.chunk.js"
+  },
+  {
+    "revision": "cfbd7e8d931fd3621614",
+    "url": "/React-Hook/static/js/main.40b45199.chunk.js"
+  },
+  {
+    "revision": "476bb76126bb1bdc7a1f",
+    "url": "/React-Hook/static/js/runtime~main.c0f1743d.js"
+  }
+]);
